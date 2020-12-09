40e25980ea8b82f5a90a1e0768ac5446014bcf5e
Sergey <chechelsk@gmail.com>
2020-12-09 14:13:42 +0200

    made some changes to the test-folder

--- test/js/script.js
+++ test/js/script.js
@@ -1,2 +1,14 @@
 const name = 'John';
+console.log($('h2'));
 $('h2').eq(0).html(`Это предложение именно для вас, ${name}.`)
+
+
+$('form').on('submit', sendForm);
+
+function sendForm(e) {
+    console.log(this);
+    e.preventDefault();
+    const formData = new FormData(this);
+    $().post('../server.php', formData)
+        .then(res => console.log(res));
+}
